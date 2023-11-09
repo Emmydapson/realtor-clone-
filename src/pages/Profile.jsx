85a1e2ccddd38479419df47f9ef5500ddcade530
emmydapson <iloriemmanuel00@gmail.com>
2023-11-09 09:23:32 +0100

    add delete and edit functionality to the listing

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { getAuth, updateProfile } from "firebase/auth";
-import { collection, doc, getDocs, orderBy, query, updateDoc, where } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, orderBy, query, updateDoc, where } from "firebase/firestore";
 import { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -68,7 +68,20 @@ export default function Profile() {
       setLoading(false);
     }
     fetchuserListings();
-  }, [auth.currentUser.uid])
+  }, [auth.currentUser.uid]);
+  async function onDelete(listingID) {
+    if (window.confirm("Are you sure you want to delete?")) {
+      await deleteDoc(doc(db, "listings", listingID))
+      const updatedListings = listings.filter(
+        (listing) => listing.id ===listingID
+      );
+      setListings(updatedListings)
+      toast.success("Successfully deleted the listing")
+    }
+  }
+  function onEdit(listingID) {
+    navigate(`/edit-listing/${listingID}`);
+  }
   return (
     <>
     <section className="max-w-6xl mx-auto flex justify-center flex-col">
@@ -132,7 +145,10 @@ export default function Profile() {
             <ListingItem
             key={listing.id}
             id={listing.id}
-            listing={listing.data}/>
+            listing={listing.data}
+            onDelete={() =>onDelete(listing.id)}
+            onEdit={() => onEdit(listing.id)}
+            />
           ))}
         </ul>
       </>
